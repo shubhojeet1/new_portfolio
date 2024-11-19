a8f08ea0233314629187e11addf5018056c9e924
coderoofsit <141501346+coderoofsit@users.noreply.github.com>
2024-11-19 18:07:36 +0530

    added resumes ones in a new window

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,13 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
-import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
-import {
-  AiFillStar,
-  AiOutlineHome,
-  AiOutlineFundProjectionScreen,
-  AiOutlineUser,
-} from "react-icons/ai";
-
 import { CgFileDocument } from "react-icons/cg";
+import {
+  AiOutlineHome,
+  AiOutlineUser,
+  AiOutlineFundProjectionScreen,
+} from "react-icons/ai";
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
@@ -30,6 +24,18 @@ function NavBar() {
 
   window.addEventListener("scroll", scrollHandler);
 
+  // Handle the resume download and open in new tab
+  const handleResumeClick = () => {
+    // Open the resume link in a new tab
+    window.open("https://drive.google.com/file/d/14t8wETIlJNYJwxcyOjPFUcsI8Q_2yBZN/view?usp=sharing", "_blank");
+
+    // Trigger the download
+    const link = document.createElement("a");
+    link.href = "https://drive.google.com/uc?export=download&id=14t8wETIlJNYJwxcyOjPFUcsI8Q_2yBZN";
+    link.download = "Resume.pdf"; // You can specify the name of the downloaded file
+    link.click();
+  };
+
   return (
     <Navbar
       expanded={expand}
@@ -39,7 +45,11 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          <img src="https://shubhojeet1.github.io/static/media/Logo.773b2970fd4e72470021.png" className="img-fluid logo" alt="brand" />
+          <img
+            src="https://shubhojeet1.github.io/static/media/Logo.773b2970fd4e72470021.png"
+            className="img-fluid logo"
+            alt="brand"
+          />
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
@@ -92,18 +102,10 @@ function NavBar() {
             </Nav.Item>
 
             <Nav.Item>
-  <Nav.Link
-    href="https://drive.google.com/uc?export=download&id=14t8wETIlJNYJwxcyOjPFUcsI8Q_2yBZN"
-    download
-    onClick={() => updateExpanded(false)}
-  >
-    <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-  </Nav.Link>
-</Nav.Item>
-
-
-
-   
+              <Nav.Link onClick={handleResumeClick}>
+                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+              </Nav.Link>
+            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
